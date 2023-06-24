5a1fc72d3e7f9ee1796df439e59eb5a4155fdb81
Mad-coder-101 <pkandibanda123@gmail.com>
2023-06-24 15:23:21 -0500

    first commit

--- LinksAndApps.py
+++ LinksAndApps.py
@@ -1,14 +1,26 @@
 import os
+import webbrowser
+import pyttsx3
+engine = pyttsx3.init()
 def openLinkorApp(words):
     if words == 'chess':
-        webbrowser.open('https://lichess.org/')
+        webbrowser.open_new('https://lichess.org/')
+        engine.say('Opening Chess')
     elif words == 'youtube':
-        webbrowser.open('https://www.youtube.com/')
+        webbrowser.open_new('https://www.youtube.com/')
+        engine.say('Opening Youtube')
     elif words == 'github':
-        webbrowser.open('https://github.com/Mad-Coder-101/')
-    elif words == 'mail' or 'gmail':
-        webbrowser.open('https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox')
-    elif words == 'vs code' or 'visual studio code' or 'code':
+        webbrowser.open_new('https://github.com/Mad-Coder-101/')
+        engine.say('Opening Github')
+    elif words == 'gmail' or words == 'mail':
+        webbrowser.open_new('https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox')
+        engine.say('Opening Mail')
+    elif words == 'vs code' or words == 'visual studio code' or words == 'code':
         os.startfile("D:\Pranav\Python\VSCODE\Code.exe")
+        engine.say('Opening V S Code')
+    elif words == 'file explorer':
+        os.startfile('Home')
+        engine.say('Opening file explorer')
+    engine.runAndWait()
 
 
--- main.py
+++ main.py
@@ -2,24 +2,36 @@ import speech_recognition as sr
 import pyttsx3
 import wikipedia
 import googlesearch
+import webbrowser
+import LinksAndApps
+import os
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 recognizer.dynamic_energy_threshold = False
 recognizer.dynamic_energy_threshold = .5
-recognizer.set_output_format("audio")
 if __name__ == '__main__':
     while True:
         with sr.Microphone() as source:
             audio = recognizer.listen(source,timeout=None)
         try:
-            words = recognizer.recognize_google(audio)
+            global words
+            words = recognizer.recognize_google(audio).lower()
             if 'quit' in words:
                 engine.stop()
                 break
-            if 'Titan' in words:
-                engine.say('Hello, how can I help you?')
-                engine.runAndWait()
-
+            if 'titan' in words:
+                words = words.replace('titan','')
+                if 'wikipedia' in words:
+                    words = words.replace('search wikipedia for', '')
+                    wikipedia.search(words)
+                elif 'Google' in words:
+                    words = words.replace('search google for', '')
+                    googlesearch.search(words)
+                elif 'open' in words:
+                    words = words.replace(' open ','')
+                    LinksAndApps.openLinkorApp(words)
+                    print(words)
+                
             else:
                 continue
         except sr.UnknownValueError:
